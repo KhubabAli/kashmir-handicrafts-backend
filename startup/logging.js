973be0cd5167b99d226cb51171bd4a63429034a7
Khubaib Ali <khubabali3061995@gmail.com>
2022-03-22 08:02:33 +0500

    Add support for ConnStr trhought environement vairable.

--- startup/logging.js
+++ startup/logging.js
@@ -1,6 +1,7 @@
 const winston = require("winston");
 require("winston-mongodb");
-// require("express-async-errors");
+require("express-async-errors");
+const config = require("config");
 
 
 module.exports = function () {
@@ -10,7 +11,7 @@ module.exports = function () {
     )
     winston.add(new winston.transports.File({filename: 'error.log'}));
     winston.add(new winston.transports.Console({colorize: true, prettyPrint: true}));
-    // winston.add(new winston.transports.MongoDB({db: "mongodb://localhost/kashmir-handicraft"}));
+    winston.add(new winston.transports.MongoDB({db: config.get("")}));
 
     process.on('uncaughtException', (error) => {
         console.log("AN UNCAUGHT EXCEPTION IS CAUGHT");
